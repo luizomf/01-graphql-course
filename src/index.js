b538a829e024b5979247552af7bb13ec439cb53f
Luiz Otávio <luizomf@gmail.com>
2021-04-27 09:31:17 -0300

    Primeira Query

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('Oi');
+import { ApolloServer, gql } from 'apollo-server';
+
+const server = new ApolloServer({
+  typeDefs: gql`
+    type Query {
+      hello: String
+    }
+  `,
+});
+
+server.listen(4003).then(({ url }) => {
+  console.log(`Server listening on url ${url}`);
+});
