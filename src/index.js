32edc2b1a40051508e9b86dfee0c006faf4a8753
Luiz Otávio <luizomf@gmail.com>
2021-04-27 09:41:54 -0300

    Primeiros resolvers

--- src/index.js
+++ src/index.js
@@ -4,8 +4,19 @@ const server = new ApolloServer({
   typeDefs: gql`
     type Query {
       hello: String
+      hi: String
     }
   `,
+  resolvers: {
+    Query: {
+      hello: async () => {
+        return 'Hello again';
+      },
+      hi: () => {
+        return 'Hi';
+      },
+    },
+  },
 });
 
 server.listen(4003).then(({ url }) => {
