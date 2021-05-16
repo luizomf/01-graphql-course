f385b34e3c88376ac888a73d73c1b1da037a9182
Luiz Otavio <luizomf@gmail.com>
2021-05-16 17:38:39 -0300

    JWT e Bcrypt - Teoria

---
+++ bcrypt-jwt.js
@@ -0,0 +1,31 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+(async () => {
+  const password = '123456';
+  const passwordHash = await bcrypt.hash(password, 12);
+
+  const userLogginPassword = '123457';
+
+  const passwordIsValid = await bcrypt.compare(
+    userLogginPassword,
+    passwordHash,
+  );
+
+  console.log(passwordIsValid);
+})();
+
+(async () => {
+  const JWT_SECRET = 'O_MEU_SECRET';
+
+  let token = jwt.sign({ userId: '123' }, JWT_SECRET, {
+    expiresIn: '7d',
+  });
+
+  const delay = () => new Promise((r) => setTimeout(r, 1000));
+  await delay();
+
+  const tokenData = jwt.verify(token, JWT_SECRET);
+
+  console.log(tokenData);
+})();
