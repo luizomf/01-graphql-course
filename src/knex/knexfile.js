3582f46cb3698f14eab893dd561d2d6399a81a22
Luiz Otavio <luizomf@gmail.com>
2021-05-30 07:00:31 -0300

    Heroku 1

--- src/knex/knexfile.js
+++ src/knex/knexfile.js
@@ -9,6 +9,8 @@ module.exports = {
   development: {
     client: process.env.DATABASE_CLIENT,
     connection: {
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
       database: process.env.DATABASE_NAME,
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
@@ -25,6 +27,8 @@ module.exports = {
   production: {
     client: process.env.DATABASE_CLIENT,
     connection: {
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
       database: process.env.DATABASE_NAME,
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
